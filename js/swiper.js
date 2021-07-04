cf1d2618696cb7a28096d99ad5302c099973345d
Natalia Sofina <nataliasofina@gmail.com>
2021-07-04 09:20:24 +0300

    Added swiper.

--- js/swiper.js
+++ js/swiper.js
@@ -1,4 +1,32 @@
-new Swiper('.slider-main__body', {
+
+
+if(document.querySelector('.slider-main__body')) {
+    const swiper = new Swiper('.slider-main__body', {
+        observer: true,
+        observeParents: true,
+        sliderPerView: 1,
+        spaceBetween: 32,
+        watchOverFlow: true,
+        speed: 800,
+        loop: true,
+        loopAditionalSliders: 5,
+        preloadImages: false,
+        parallax: true,
+        // dots
+        pagination: {
+            el: '.controls-slider-main__dotts',
+            clickable: true,
+        },
+        // arrows
+        navigation: {
+            nextEl: '.slider-main .slider-arrow_next',
+            prevEl: '.slider-main .slider-arrow_prev',
+        }
+    });
+}
+
+/* 
+, {
     observer: true,
     observeParents: true,
     slidesPerView: 1,
@@ -10,4 +38,4 @@ new Swiper('.slider-main__body', {
         nextEl: '.slider-arrow_prev',
         prevEl: '.slider-arrow_next'
     },
-});
+}*/
