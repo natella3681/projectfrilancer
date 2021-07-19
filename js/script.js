68b19534c9b39b611c4709528ccb4fbb626c69a6
Natalia Sofina <nataliasofina@gmail.com>
2021-07-19 20:56:01 +0300

    commit message

--- js/script.js
+++ js/script.js
@@ -31,7 +31,37 @@ window.onload = function() {
             getProducts(targetElement);
             e.preventDefault();
         }
+
+        if(targetElement.classList.contains('actions-product__button')) {
+            const productId = targetElement.closest('.item-product').dataset.pid;
+            addToCart(targetElement, productId);
+            e.preventDefault();
+        }
     }
+
+    // ibg и адаптация картинки 
+
+    function isIE() {
+        const ua = navigator.userAgent;
+        var is_ie = ua.indexOf("MSIE ") > -1 || ua.indexOf("Trident/") > -1;
+        return is_ie;
+    }
+    if (isIE()) {
+        document.querySelector('html').classList.add('ie');
+    }
+
+    function ibg() {
+        if (isIE()) {
+            let ibg = document.querySelectorAll("._ibg");
+            for (var i = 0; i < ibg.length; i++) {
+                if (ibg[i].querySelector('img') && ibg[i].querySelector('img').getAttribute('src') != null) {
+                    ibg[i].style.backgroundImage = 'url(' + ibg[i].querySelector('img').getAttribute('src') + ')';
+                }
+            }
+        }
+    }
+    ibg();
+
 
     // бургер меню
     const iconMenu = document.querySelector('.icon-menu');
@@ -193,6 +223,95 @@ window.onload = function() {
             productsItems.insertAdjacentHTML('beforeend', productTemplate);
         })
     }
+
+    // Add to cart
+    function addToCart(productButton, productId) {
+        if(!productButton.classList.contains('_hold')) {
+            productButton.classList.add('_hold');
+            productButton.classList.add('_fly');
+
+            const cart = document.querySelector('.cart-header__icon');
+           // const product = document.querySelector(`[data-pid="${productId}"]`);
+            const productImage = document.querySelector(`[data-pid="${productId}"] .item-product__image`);
+
+            const productImageFly = productImage.cloneNode(true);
+
+            const productImageFlyWidth = productImage.offsetWidth;
+            const productImageFlyHeight = productImage.offsetHeight;
+            const productImageFlyTop = productImage.getBoundingClientRect().top;
+            const productImageFlyLeft = productImage.getBoundingClientRect().left;
+
+            productImageFly.setAttribute('class', '_flyImage _ibg');
+            productImageFly.style.cssText = 
+            `
+            left: ${productImageFlyLeft}px;
+            top: ${productImageFlyTop}px;
+            width: ${productImageFlyWidth}px;
+            height: ${productImageFlyHeight}px;
+            `;
+
+            document.body.append(productImageFly);
+
+            const cartFlyLeft = cart.getBoundingClientRect().left;
+            const cartFlyTop = cart.getBoundingClientRect().top;
+
+            productImageFly.style.cssText = 
+            `
+            left: ${cartFlyLeft}px;
+            top: ${cartFlyTop}px;
+            width: 0px;
+            height: 0px;
+            opacity: 0;
+            `;
+
+            productImageFly.addEventListener('transitionend', function() {
+                if(productButton.classList.contains('_fly')) {
+                    productImageFly.remove();
+                    updateCart(productButton, productId);
+                    productButton.classList.remove('_fly');
+                }
+            })
+        }
+    }
+
+    function updateCart(productButton, productId, productAdd = true) {
+        const cart = document.querySelector('.cart-header');
+        const cartIcon = cart.querySelector('.cart-header__icon');
+        const cartQuantity = cartIcon.querySelector('span');
+        const cartProduct = document.querySelector(`[data-cart-pid="${productId}"]`);
+        const cartList = document.querySelector('.cart-list');
+
+        // Добавление в корзину
+        if(productAdd) {
+            if(cartQuantity) {
+                cartQuantity.innerHTML = ++cartQuantity.innerHTML;
+            } else {
+                cartIcon.insertAdjacentHTML('beforeend', `<span>1</span>`);
+            }
+            if(!cartProduct){
+                const product = document.querySelector(`[data-pid="${productId}"]`);
+                const cartProductImage = product.querySelector('.item-product__image').innerHTML;
+                const cartProductTitle = product.querySelector('.item-product__title').innerHTML;
+                const cartProductContent = `
+                <a href="" class="cart-list__image _ibg">${cartProductImage}</a>
+                <div class="cart-list__body">
+                    <a href="" class="cart-list__title">${cartProductTitle}</a>
+                    <div class="cart-list__quantity">Quantity: <span></span></div>
+                    <a href="" class="cart-list__delete">Delete</a>
+                </div>
+                `;
+                cartList.insertAdjacentHTML('beforeend', `<li data-cart-pid="${productId}" class="cart-list__item">${cartProductContent}</li>`)
+            } else {
+                const cartProductQuantity = cartProduct.querySelector('.cart-list__quantity span');
+                cartProductQuantity.innerHTML = ++cartProductQuantity.innerHTML;
+            }
+
+            // после всех действий 
+            productButton.classList.remove('_hold');
+        }
+    }
+
+    
 
     /*var requestURL = 'json/products.json';
     var request = new XMLHttpRequest();
