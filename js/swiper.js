fbe9e28143c944250b1bb7a05e09c2fbebba1949
Natalia Sofina <nataliasofina@gmail.com>
2021-07-26 08:34:35 +0300

    Добавила два слайдера

--- js/swiper.js
+++ js/swiper.js
@@ -26,17 +26,66 @@ if(document.querySelector('.slider-main__body')) {
     });
 }
 
-/* 
-, {
-    observer: true,
-    observeParents: true,
-    slidesPerView: 1,
-    spaceBetween: 32,
-    watchOverFlow: true,
-    speed: 800,
-    loop: true,
-    navigation: {
-        nextEl: '.slider-arrow_prev',
-        prevEl: '.slider-arrow_next'
-    },
-}*/
+if(document.querySelector('.slider-rooms__body')) {
+    const swiper = new Swiper('.slider-rooms__body', {
+        observer: true,
+        observeParents: true,
+        slidesPerView: 'auto',
+        spaceBetween: 24,
+        watchOverFlow: true,
+        speed: 800,
+        loop: true,
+        loopAditionalSliders: 5,
+        preloadImages: false,
+        parallax: true,
+        //centeredSlides: true,
+        // dots
+        pagination: {
+            el: '.slider-rooms__dotts',
+            clickable: true,
+        },
+        // arrows
+        navigation: {
+            nextEl: '.slider-rooms .slider-arrow_next',
+            prevEl: '.slider-rooms .slider-arrow_prev',
+        }
+    });
+}
+
+if(document.querySelector('.slider-tips__body')) {
+    const swiper = new Swiper('.slider-tips__body', {
+        observer: true,
+        observeParents: true,
+        slidesPerView: 3,
+        spaceBetween: 32,
+        speed: 800,
+        loop: true,
+        watchOverFlow: true,
+        // dots
+        pagination: {
+            el: '.slider-tips__dotts',
+            clickable: true,
+        },
+        // arrows
+        navigation: {
+            nextEl: '.slider-tips .slider-arrow_next',
+            prevEl: '.slider-tips .slider-arrow_prev',
+        },
+        breakpoints: {
+            320: {
+                slidesPerView: 1.1,
+                spaceBetween: 13,
+            },
+            768: {
+                slidesPerView: 2,
+                spaceBetween: 20,
+            },
+            992: {
+                slidesPerView: 3,
+                spaceBetween: 32,
+            }
+        }
+    });
+}
+
+
